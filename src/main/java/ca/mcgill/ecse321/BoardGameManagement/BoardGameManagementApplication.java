2df10d9d1b092e2dc43f927a9af9ab4c7920a15f
Tingyicty <tingyi.chen@mail.mcgill.ca>
2025-02-06 14:19:04 -0500

    adding spring boot

---
+++ src/main/java/ca/mcgill/ecse321/BoardGameManagement/BoardGameManagementApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.BoardGameManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGameManagementApplication.class, args);
+	}
+
+}
