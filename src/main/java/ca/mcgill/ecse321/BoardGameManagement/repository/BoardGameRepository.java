e301d572c270df272470b31b6786fd95e8ef92d8
HachemJ <jadd.lhm@gmail.com>
2025-02-13 10:55:11 -0500

    Added CRUD repos

---
+++ src/main/java/ca/mcgill/ecse321/BoardGameManagement/repository/BoardGameRepository.java
@@ -0,0 +1,7 @@
+package ca.mcgill.ecse321.BoardGameManagement.repository;
+import ca.mcgill.ecse321.BoardGameManagement.model.BoardGame;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BoardGameRepository extends CrudRepository<BoardGame, Integer> {
+  public BoardGame findByGameID(int gameID);
+}
