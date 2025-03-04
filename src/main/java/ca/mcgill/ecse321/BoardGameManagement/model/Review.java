cc2332b0ac819906270d296a1b215259b6c90953
HachemJ <jadd.lhm@gmail.com>
2025-03-04 16:12:41 -0500

    Remove autogenerated comments from umple models

--- src/main/java/ca/mcgill/ecse321/BoardGameManagement/model/Review.java
+++ src/main/java/ca/mcgill/ecse321/BoardGameManagement/model/Review.java
@@ -1,27 +1,14 @@
 package ca.mcgill.ecse321.BoardGameManagement.model;
 
-/*PLEASE DO NOT EDIT THIS CODE*/
-/*This code was generated using the UMPLE 1.35.0.7523.c616a4dce modeling language!*/
-
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-
 import java.sql.Date;
 
-// line 42 "model.ump"
-// line 97 "model.ump"
 @Entity
-public class Review
-{
+public class Review {
 
-  //------------------------
-  // MEMBER VARIABLES
-  //------------------------
-
-  //Review Attributes
   @Id
   @GeneratedValue
   private int reviewID;
@@ -29,131 +16,93 @@ public class Review
   private String comment;
   private Date commentDate;
 
-  //Review Associations
   @ManyToOne
-
   private Player author;
-  @ManyToOne
 
+  @ManyToOne
   private BoardGame boardGame;
 
-  //------------------------
-  // CONSTRUCTOR
-  //------------------------
-  public Review() {
+  public Review() {}
 
-  }
-
-  public Review(int aRating, String aComment, Date aCommentDate, Player aAuthor, BoardGame aBoardGame)
-  {
+  public Review(int aRating, String aComment, Date aCommentDate, Player aAuthor, BoardGame aBoardGame) {
     rating = aRating;
     comment = aComment;
     commentDate = aCommentDate;
-    if (!setAuthor(aAuthor))
-    {
-      throw new RuntimeException("Unable to create Review due to aAuthor. See https://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
+    if (!setAuthor(aAuthor)) {
+      throw new RuntimeException("Unable to create Review due to aAuthor.");
     }
-    if (!setBoardGame(aBoardGame))
-    {
-      throw new RuntimeException("Unable to create Review due to aBoardGame. See https://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
+    if (!setBoardGame(aBoardGame)) {
+      throw new RuntimeException("Unable to create Review due to aBoardGame.");
     }
   }
 
-  //------------------------
-  // INTERFACE
-  //------------------------
-
-  public boolean setRating(int aRating)
-  {
-    boolean wasSet = false;
+  public boolean setRating(int aRating) {
     rating = aRating;
-    wasSet = true;
-    return wasSet;
+    return true;
   }
 
-  public boolean setComment(String aComment)
-  {
-    boolean wasSet = false;
+  public boolean setComment(String aComment) {
     comment = aComment;
-    wasSet = true;
-    return wasSet;
+    return true;
   }
 
-  public boolean setCommentDate(Date aCommentDate)
-  {
-    boolean wasSet = false;
+  public boolean setCommentDate(Date aCommentDate) {
     commentDate = aCommentDate;
-    wasSet = true;
-    return wasSet;
+    return true;
   }
 
-  public int getReviewID()
-  {
+  public int getReviewID() {
     return reviewID;
   }
 
-  public int getRating()
-  {
+  public int getRating() {
     return rating;
   }
 
-  public String getComment()
-  {
+  public String getComment() {
     return comment;
   }
 
-  public Date getCommentDate()
-  {
+  public Date getCommentDate() {
     return commentDate;
   }
-  /* Code from template association_GetOne */
-  public Player getAuthor()
-  {
+
+  public Player getAuthor() {
     return author;
   }
-  /* Code from template association_GetOne */
-  public BoardGame getBoardGame()
-  {
+
+  public BoardGame getBoardGame() {
     return boardGame;
   }
-  /* Code from template association_SetUnidirectionalOne */
-  public boolean setAuthor(Player aNewAuthor)
-  {
-    boolean wasSet = false;
-    if (aNewAuthor != null)
-    {
+
+  public boolean setAuthor(Player aNewAuthor) {
+    if (aNewAuthor != null) {
       author = aNewAuthor;
-      wasSet = true;
+      return true;
     }
-    return wasSet;
-  }
-  /* Code from template association_SetUnidirectionalOne */
-  public boolean setBoardGame(BoardGame aNewBoardGame)
-  {
-    boolean wasSet = false;
-    if (aNewBoardGame != null)
-    {
-      boardGame = aNewBoardGame;
-      wasSet = true;
-    }
-    return wasSet;
+    return false;
   }
 
-  public void delete()
-  {
+  public boolean setBoardGame(BoardGame aNewBoardGame) {
+    if (aNewBoardGame != null) {
+      boardGame = aNewBoardGame;
+      return true;
+    }
+    return false;
+  }
+
+  public void delete() {
     author = null;
     boardGame = null;
   }
 
-
-  public String toString()
-  {
+  public String toString() {
     return super.toString() + "["+
         "reviewID" + ":" + getReviewID()+ "," +
         "rating" + ":" + getRating()+ "," +
-        "comment" + ":" + getComment()+ "]" + System.getProperties().getProperty("line.separator") +
-        "  " + "commentDate" + "=" + (getCommentDate() != null ? !getCommentDate().equals(this)  ? getCommentDate().toString().replaceAll("  ","    ") : "this" : "null") + System.getProperties().getProperty("line.separator") +
-        "  " + "author = "+(getAuthor()!=null?Integer.toHexString(System.identityHashCode(getAuthor())):"null") + System.getProperties().getProperty("line.separator") +
+        "comment" + ":" + getComment()+ "]" + System.lineSeparator() +
+        "  " + "commentDate" + "=" + (getCommentDate() != null ? getCommentDate().toString() : "null") + System.lineSeparator() +
+        "  " + "author = "+(getAuthor()!=null?Integer.toHexString(System.identityHashCode(getAuthor())):"null") + System.lineSeparator() +
         "  " + "boardGame = "+(getBoardGame()!=null?Integer.toHexString(System.identityHashCode(getBoardGame())):"null");
   }
 }
