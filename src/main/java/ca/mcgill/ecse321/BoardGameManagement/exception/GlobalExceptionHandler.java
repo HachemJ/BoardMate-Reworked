af32a228794c099c0223c635caaf979534f3327e
Nizar <masplong@gmail.com>
2025-03-08 15:58:57 -0500

    Adding EventService, EventController and tests

---
+++ src/main/java/ca/mcgill/ecse321/BoardGameManagement/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package ca.mcgill.ecse321.BoardGameManagement.exception;
+
+import ca.mcgill.ecse321.BoardGameManagement.dto.ErrorDto;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+  @ExceptionHandler(GlobalException.class)
+  public ResponseEntity<ErrorDto> handleGlobalException(GlobalException e) {
+    return new ResponseEntity<ErrorDto>(new ErrorDto(e.getMessage()), e.getStatus());
+  }
+
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<ErrorDto> handleConstraintViolationException(ConstraintViolationException e) {
+    List<String> errors = new ArrayList<>();
+    for (ConstraintViolation<?> cv : e.getConstraintViolations()) {
+      errors.add(cv.getMessage());
+    }
+    return new ResponseEntity<ErrorDto>(new ErrorDto(errors), HttpStatus.BAD_REQUEST);
+  }
+}
