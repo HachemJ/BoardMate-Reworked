0e10767dfbf995159d6d535d18c0239041e6e030
Alexander Fou <alexander.fou@mail.mcgill.ca>
2025-03-12 14:47:14 -0400

    Finished DTOs

--- src/main/java/ca/mcgill/ecse321/BoardGameManagement/dto/ReviewResponseDto.java
+++ src/main/java/ca/mcgill/ecse321/BoardGameManagement/dto/ReviewResponseDto.java
@@ -1,5 +1,42 @@
 package ca.mcgill.ecse321.BoardGameManagement.dto;
 
-public class ReviewResponseDto {
+import java.sql.Date;
 
+public class ReviewResponseDto {
+    private int reviewID;
+    private int rating;
+    private String comment;
+    private Date commentDate;
+
+    public ReviewResponseDto() {}
+
+    public ReviewResponseDto(ReviewResponseDto review) {
+        this.reviewID = review.getReviewID();
+        this.rating = review.getRating();
+        this.comment = review.getComment();
+        this.commentDate = review.getCommentDate();
+    }
+
+    public ReviewResponseDto(int reviewID, int rating, String comment, Date commentDate) {
+        this.reviewID = reviewID;
+        this.rating = rating;
+        this.comment = comment;
+        this.commentDate = commentDate;
+    }
+
+    public int getReviewID() {
+        return reviewID;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public Date getCommentDate() {
+        return commentDate;
+    }
 }
