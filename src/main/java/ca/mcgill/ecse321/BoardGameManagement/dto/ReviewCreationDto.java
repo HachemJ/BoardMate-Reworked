620dcca4e1c7553c36b1a714a6fae143e9a3322b
Alexander Fou <alexander.fou@mail.mcgill.ca>
2025-03-12 17:16:37 -0400

    Added review controller

--- src/main/java/ca/mcgill/ecse321/BoardGameManagement/dto/ReviewCreationDto.java
+++ src/main/java/ca/mcgill/ecse321/BoardGameManagement/dto/ReviewCreationDto.java
@@ -2,11 +2,16 @@ package ca.mcgill.ecse321.BoardGameManagement.dto;
 
 import java.sql.Date;
 
+import ca.mcgill.ecse321.BoardGameManagement.model.BoardGame;
+import ca.mcgill.ecse321.BoardGameManagement.model.Player;
+
 public class ReviewCreationDto {
     private int reviewID;
     private int rating;
     private String comment;
     private Date commentDate;
+    private Player author;
+    private BoardGame boardGame;
 
     public ReviewCreationDto() {}
 
@@ -15,13 +20,17 @@ public class ReviewCreationDto {
         this.rating = review.getRating();
         this.comment = review.getComment();
         this.commentDate = review.getCommentDate();
+        this.author = review.getAuthor();
+        this.boardGame = review.getBoardGame();
     }
 
-    public ReviewCreationDto(int reviewID, int rating, String comment, Date commentDate) {
+    public ReviewCreationDto(int reviewID, int rating, String comment, Date commentDate, Player author, BoardGame boardGame) {
         this.reviewID = reviewID;
         this.rating = rating;
         this.comment = comment;
         this.commentDate = commentDate;
+        this.author = author;
+        this.boardGame = boardGame;
     }
 
     public int getReviewID() {
@@ -38,6 +47,14 @@ public class ReviewCreationDto {
 
     public Date getCommentDate() {
         return commentDate;
+    }
+
+    public Player getAuthor() {
+        return author;
+    }
+
+    public BoardGame getBoardGame() {
+        return boardGame;
     }
 
 }
---
+++ src/main/java/ca/mcgill/ecse321/BoardGameManagement/service/ReviewService.java
@@ -0,0 +1,98 @@
+package ca.mcgill.ecse321.BoardGameManagement.service;
+
+import ca.mcgill.ecse321.BoardGameManagement.dto.ReviewCreationDto;
+import ca.mcgill.ecse321.BoardGameManagement.exception.GlobalException;
+import ca.mcgill.ecse321.BoardGameManagement.model.*;
+import ca.mcgill.ecse321.BoardGameManagement.repository.*;
+
+import jakarta.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+
+import java.util.ArrayList;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    @Autowired
+    private BoardGameRepository boardGameRepository;
+
+    public ReviewService() {}
+
+    /*
+    public ArrayList<Review> getReviewsForGame(int gameId) {
+        BoardGame boardGame = boardGameRepository.findById(gameId).orElseThrow();
+    }
+    */
+
+    /**
+     * Creates a review in the database
+     */
+    @Transactional
+    public Review createReview(ReviewCreationDto reviewDto) {
+        
+        Review review = new Review(reviewDto.getRating(), reviewDto.getComment(), reviewDto.getCommentDate(), reviewDto.getAuthor(), reviewDto.getBoardGame());
+        return reviewRepository.save(review);
+    }
+
+    /**
+     * Updates a review in the database, based on its ID
+     */
+    @Transactional
+    public Review updateReview(int reviewId, ReviewCreationDto reviewDto) {
+        Review review = reviewRepository.findById(reviewId).orElse(null);
+        if (review == null) {
+            throw new GlobalException(HttpStatus.NOT_FOUND, "Review not found with ID: " + reviewId);
+        }
+        review.setRating(reviewDto.getRating());
+        review.setComment(reviewDto.getComment());
+        review.setCommentDate(reviewDto.getCommentDate());
+        review.setAuthor(reviewDto.getAuthor());
+        review.setBoardGame(reviewDto.getBoardGame());
+
+        return reviewRepository.save(review);
+    }
+
+    /**
+     * Returns all reviews from the database
+     */
+    @Transactional
+    public ArrayList<Review> getAllReviews() {
+        return (ArrayList<Review>) reviewRepository.findAll();
+    }
+
+    /**
+     * Returns a review from the database, based on its ID
+     */
+    @Transactional
+    public Review getReviewById(int reviewId) {
+        Review review = reviewRepository.findById(reviewId).orElse(null);
+        if (review == null) {
+            throw new GlobalException(HttpStatus.NOT_FOUND, "Review not found with ID: " + reviewId);
+        }
+
+        return review;
+    }
+    
+    /**
+     * Deletes a review from the database, based on its ID
+     */
+    @Transactional
+    public void deleteReview(int reviewId) {
+        Review review = reviewRepository.findById(reviewId).orElse(null);
+        if (review == null) {
+            throw new GlobalException(HttpStatus.NOT_FOUND, "Review not found with ID: " + reviewId);
+        }
+        reviewRepository.delete(review);
+    }
+}
