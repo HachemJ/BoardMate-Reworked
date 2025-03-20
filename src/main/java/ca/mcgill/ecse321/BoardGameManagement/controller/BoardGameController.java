9c461f71fdd44e8a9a885949e99f4a530b54cf10
HachemJ <jadd.lhm@gmail.com>
2025-03-19 21:43:18 -0400

    added javadoc to BoardGameService and BoardGameController methods

--- src/main/java/ca/mcgill/ecse321/BoardGameManagement/controller/BoardGameController.java
+++ src/main/java/ca/mcgill/ecse321/BoardGameManagement/controller/BoardGameController.java
@@ -18,6 +18,11 @@ public class BoardGameController {
   @Autowired
   private BoardGameService boardGameService;
 
+  /**
+   * This method creates a new BoardGame object and returns a BoardGameResponseDto object.
+   * @param boardGameDto the DTO that contains the information to create a new BoardGame object
+   * @return BoardGameResponseDto
+   */
   @PostMapping("/boardgames")
   @ResponseStatus(HttpStatus.CREATED)
   public BoardGameResponseDto createBoardGame(@RequestBody BoardGameCreationDto boardGameDto) {
@@ -25,6 +30,10 @@ public class BoardGameController {
     return new BoardGameResponseDto(boardGame);
   }
 
+  /**
+   * This method retrieves all BoardGame objects and returns a list of BoardGameResponseDto objects.
+   * @return List of BoardGameResponseDto
+   */
   @GetMapping("/boardgames")
   public List<BoardGameResponseDto> getAllBoardGames() {
     List<BoardGame> boardGames = boardGameService.getAllBoardGames();
@@ -33,18 +42,33 @@ public class BoardGameController {
         .collect(Collectors.toList());
   }
 
+  /**
+   * This method retrieves a specific BoardGame object by its ID and returns a BoardGameResponseDto object.
+   * @param gameId the ID of the BoardGame object to retrieve
+   * @return BoardGameResponseDto
+   */
   @GetMapping("/boardgames/{gameId}")
   public BoardGameResponseDto getBoardGame(@PathVariable int gameId) {
     BoardGame boardGame = boardGameService.getBoardGameById(gameId);
     return new BoardGameResponseDto(boardGame);
   }
 
+  /**
+   * This method updates an existing BoardGame object and returns a BoardGameResponseDto object.
+   * @param gameId the ID of the BoardGame object to update
+   * @param boardGameDto the DTO containing the updated information
+   * @return BoardGameResponseDto
+   */
   @PutMapping("/boardgames/{gameId}")
   public BoardGameResponseDto updateBoardGame(@PathVariable int gameId, @RequestBody BoardGameCreationDto boardGameDto) {
     BoardGame updatedBoardGame = boardGameService.updateBoardGame(gameId, boardGameDto);
     return new BoardGameResponseDto(updatedBoardGame);
   }
 
+  /**
+   * This method deletes a BoardGame object by its ID.
+   * @param gameId the ID of the BoardGame object to delete
+   */
   @DeleteMapping("/boardgames/{gameId}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void deleteBoardGame(@PathVariable int gameId) {
