cc160eb3e7828ed5a427c5b38e524a171dbf3984
Tingyicty <tingyi.chen@mail.mcgill.ca>
2025-03-31 15:52:05 -0400

    Enable CORS

--- BoardGameManagement-Backend/src/main/java/ca/mcgill/ecse321/BoardGameManagement/controller/ReviewController.java
+++ BoardGameManagement-Backend/src/main/java/ca/mcgill/ecse321/BoardGameManagement/controller/ReviewController.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
  * This class is the controller for the Review entity. It is responsible for handling requests related to Reviews.
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @SuppressWarnings("unused")
 public class ReviewController {
 
