eecb2898583de9f5386b95a94784bacc050a1b36
HachemJ <jadd.lhm@gmail.com>
2025-04-02 16:01:58 -0400

    Passing ID through login

--- BoardGameManagement-Frontend/src/views/LandingPages/SignIn/BasicView.vue
+++ BoardGameManagement-Frontend/src/views/LandingPages/SignIn/BasicView.vue
@@ -52,7 +52,7 @@ async function handleAuth() {
       });
 
       const user = res.data;
-      authStore.login(user.name, user.email, user.isAOwner);
+      authStore.login(user.id, user.name, user.email, user.isAOwner);
 
       alert("Logged in successfully!");
       console.log("Logged in user:", authStore.user);
