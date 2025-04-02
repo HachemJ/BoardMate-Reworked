f661e6bd861279691a87d738669fb9e0ef91bd60
HachemJ <jadd.lhm@gmail.com>
2025-04-02 14:25:54 -0400

    Brought back regular profile page, and populated fields

--- BoardGameManagement-Frontend/src/stores/authStore.js
+++ BoardGameManagement-Frontend/src/stores/authStore.js
@@ -2,28 +2,29 @@ import { ref, watch } from "vue";
 import { defineStore } from "pinia";
 
 export const useAuthStore = defineStore("auth", () => {
-  // Load from localStorage or use default
   const storedUser = JSON.parse(localStorage.getItem("authUser")) || {
     username: null,
     email: null,
+    isAOwner: false,
     isAuthenticated: false,
   };
 
   const user = ref(storedUser);
 
-  function login(username, email) {
+  function login(username, email, isAOwner) {
     user.value.username = username;
     user.value.email = email;
+    user.value.isAOwner = isAOwner;
     user.value.isAuthenticated = true;
   }
 
   function logout() {
     user.value.username = null;
     user.value.email = null;
+    user.value.isAOwner = false;
     user.value.isAuthenticated = false;
   }
 
-  // Persist every time user changes
   watch(
     user,
     (newVal) => {
