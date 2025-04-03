bd351af8894c9bd99775dc2f4b41f7290c730d87
Nizar <masplong@gmail.com>
2025-04-03 19:20:53 -0400

    Fixing update event

--- BoardGameManagement-Frontend/src/examples/navbars/NavbarDefault.vue
+++ BoardGameManagement-Frontend/src/examples/navbars/NavbarDefault.vue
@@ -143,6 +143,18 @@ const isOwner = ref(authStore.user.isAOwner);  // This will control if the Route
       </button>
       <div class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0" id="navigation">
         <ul class="navbar-nav navbar-nav-hover ms-auto">
+
+          <li class="nav-item dropdown dropdown-hover mx-2" v-if="authStore.user.isAuthenticated">
+            <RouterLink
+              :to="{ name: 'profile' }"
+              class="nav-link d-flex cursor-pointer align-items-center"
+            >
+              <i class="material-icons opacity-6 me-2 text-md" :class="getTextColor()">face</i>
+
+              My Profile
+            </RouterLink>
+          </li>
+
           <li class="nav-item dropdown dropdown-hover mx-2">
             <a
               role="button"
@@ -165,9 +177,7 @@ const isOwner = ref(authStore.user.isAOwner);  // This will control if the Route
               <RouterLink :to="{ name: 'event' }" class="dropdown-item border-radius-md">
                 <span>Event Menu</span>
               </RouterLink>
-              <RouterLink :to="{ name: 'profile' }" class="dropdown-item border-radius-md">
-                <span>Profile Menu</span>
-              </RouterLink>
+
 
 
               <RouterLink
@@ -214,7 +224,6 @@ const isOwner = ref(authStore.user.isAOwner);  // This will control if the Route
               <i class="material-icons opacity-6 me-2 text-md" :class="getTextColor()">article</i>
               Contact Us
             </RouterLink>
-
           </li>
 
           <li class="nav-item dropdown dropdown-hover mx-2">
@@ -222,23 +231,13 @@ const isOwner = ref(authStore.user.isAOwner);  // This will control if the Route
               :to="{ name: 'about' }"
               class="nav-link d-flex cursor-pointer align-items-center"
             >
-              <svg
-                width="20px"
-                height="20px"
-                class="material-icons me-2 opacity-6"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-                data-testid="GitHubIcon"
-                :fill="props.transparent && '#fff'"
-              >
-                <path
-                  d="M12 1.27a11 11 0 00-3.48 21.46c.55.09.73-.28.73-.55v-1.84c-3.03.64-3.67-1.46-3.67-1.46-.55-1.29-1.28-1.65-1.28-1.65-.92-.65.1-.65.1-.65 1.1 0 1.73 1.1 1.73 1.1.92 1.65 2.57 1.2 3.21.92a2 2 0 01.64-1.47c-2.47-.27-5.04-1.19-5.04-5.5 0-1.1.46-2.1 1.2-2.84a3.76 3.76 0 010-2.93s.91-.28 3.11 1.1c1.8-.49 3.7-.49 5.5 0 2.1-1.38 3.02-1.1 3.02-1.1a3.76 3.76 0 010 2.93c.83.74 1.2 1.74 1.2 2.94 0 4.21-2.57 5.13-5.04 5.4.45.37.82.92.82 2.02v3.03c0 .27.1.64.73.55A11 11 0 0012 1.27"
-                ></path>
-              </svg>
+              <i class="material-icons opacity-6 me-2 text-md" :class="getTextColor()">group</i>
               About Us
             </RouterLink>
-
           </li>
+
+
+
         </ul>
         <ul class="navbar-nav d-lg-block d-none">
           <li class="nav-item" v-if="authStore.user.isAuthenticated">
